60bbdade03b090896ffc81f0ebfbfc42f31e5158
Jonathan Whiting <jonathan@ignika.com>
2015-08-22 22:51:37 +0100

    Finished filling out tail data

--- src/game/snake.c
+++ src/game/snake.c
@@ -219,31 +219,31 @@ void game_snake_draw(game_snake snake)
 		in_dir = whitgl_ivec_to_facing(whitgl_ivec_sub(snake.pos[snake.size-1],snake.old_pos));
 		out_dir = whitgl_ivec_to_facing(whitgl_ivec_sub(snake.pos[snake.size-2],snake.pos[snake.size-1]));
 	}
+	whitgl_int data_index = in_dir+out_dir*4;
+	const tail_data all_tail_data[16] =
+	{
+		{  VALID, NARR, HORI, { 0, 0}, { 0, 0}}, // 0, 0
+		{  VALID, WIDE, VERT, {48,64}, {-8, 0}}, // 1, 0
+		{INVALID, NARR, VERT, { 0, 0}, { 0, 0}}, // 2, 0  INVALID
+		{  VALID, WIDE, VERT, {48,32}, { 0, 0}}, // 3, 0
+		{  VALID, NARR, HORI, { 0,32}, { 0, 0}}, // 0, 1
+		{  VALID, WIDE, HORI, { 0,16}, {-8, 0}}, // 1, 1
+		{  VALID, NARR, HORI, { 0,64}, { 0,-8}}, // 2, 1
+		{INVALID, NARR, VERT, { 0, 0}, { 0, 0}}, // 3, 1  INVALID
+		{INVALID, NARR, VERT, { 0, 0}, { 0, 0}}, // 0, 2  INVALID
+		{  VALID, WIDE, VERT, {32,32}, {-8, 0}}, // 1, 2
+		{  VALID, NARR, HORI, {32, 0}, { 0,-8}}, // 2, 2
+		{  VALID, WIDE, VERT, {32,64}, { 0, 0}}, // 3, 2
+		{  VALID, NARR, HORI, { 0,80}, { 0, 0}}, // 0, 3
+		{INVALID, NARR, VERT, { 0, 0}, { 0, 0}}, // 1, 3 INVALID
+		{  VALID, NARR, HORI, { 0,48}, { 0,-8}}, // 2, 3
+		{  VALID, WIDE, HORI, { 0,24}, { 0, 0}}, // 3, 3
+	};
+
 	draw_pos.y += snake.fall_timer*8;
 	whitgl_int iframe = snake.t*3.99;
 	frame = whitgl_ivec_zero;
-	WHITGL_LOG("in_dir %d out_dir %d frame %d", in_dir, out_dir, iframe);
-	tail_data tdata = tail_data_zero;
-	if(in_dir == 0 && out_dir == 0)
-	{
-		tail_data data = {VALID, NARR, HORI, {0,0}, {0,0}};
-		tdata = data;
-	}
-	if(in_dir == 1 && out_dir == 0)
-	{
-		tail_data data = {VALID, WIDE, VERT, {48,64}, {-8,0}};
-		tdata = data;
-	}
-	if(in_dir == 1 && out_dir == 1)
-	{
-		tail_data data = {VALID, WIDE, HORI, {0,16}, {-8,0}};
-		tdata = data;
-	}
-	if(in_dir == 0 && out_dir == 1)
-	{
-		tail_data data = {VALID, NARR, HORI, {0,32}, {0,0}};
-		tdata = data;
-	}
+	tail_data tdata = all_tail_data[data_index];
 
 	if(tdata.wide)
 		tail_sprite.size.x *= 2;
